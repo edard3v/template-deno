6c24e1dc0b2a6ae2366029c2b782aa8e508a44d9
edard3v <edard3v@gmail.com>
2025-02-17 06:17:10 -0500

    agg cors

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,8 +4,11 @@ import { not_found_handler } from "./not_found_handler.ts";
 import { err_handler } from "./err_handler.ts";
 import { start_register_module } from "../modules/auth/start_register/start_register_module.ts";
 import { end_register_module } from "../modules/auth/end_register/end_register_module.ts";
+import { CORS } from "./config.ts";
 
 export const app = new Hono();
+
+app.use("/*", CORS);
 
 app.route("/", welcome_module);
 app.route("/start_register", start_register_module);
--- src/app/config.ts
+++ src/app/config.ts
@@ -1,2 +1,10 @@
+import { cors } from "hono/cors";
+
 export const PORT = Number(Deno.env.get("PORT")) || 3001;
 export const IS_PRODUCTION = Deno.env.get("DENO_ENV") === "production";
+export const CORS = cors({
+  origin: ["http://localhost:5173"],
+  allowMethods: ["POST", "GET", "UPDATE", "PUT", "DELETE"],
+  maxAge: 600,
+  credentials: true,
+});
