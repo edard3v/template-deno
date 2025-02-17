5615171aab2d16692da19f07ac62ea0bd1f5de3f
edard3v <edard3v@gmail.com>
2025-02-17 01:42:55 -0500

    agg middle verify auth

---
+++ src/middlewares/verify_auth.ts
@@ -0,0 +1,24 @@
+import type { MiddlewareHandler } from "hono";
+import { JWT } from "../services/tokens/jwt.ts";
+import type { JwtPayload } from "jsonwebtoken";
+import { Unauthorized } from "../errors/unauthorized.ts";
+import { Bearer_err } from "../errors/bearer_err.ts";
+
+export const verify_auth: MiddlewareHandler<T> = async (context, next) => {
+  const Authorization = context.req.header("Authorization");
+  if (!Authorization) throw new Unauthorized();
+
+  const [prefix, token] = Authorization.split(" ");
+  if (prefix !== "Bearer") throw new Bearer_err();
+  if (!token) throw new Unauthorized();
+
+  const tokenPayload = JWT.verify(token) as JwtPayload;
+
+  context.set("tokenPayload", tokenPayload);
+
+  await next();
+};
+
+type T = {
+  Variables: { tokenPayload: JwtPayload };
+};
