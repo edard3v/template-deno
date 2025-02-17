16b319eefc99d9c204074257385c7233852caeb3
edard3v <edard3v@gmail.com>
2025-02-17 06:13:30 -0500

    registro y verificación funcional

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,2 +1,2 @@
-export const PORT = Number(Deno.env.get("PORT")) || 3000;
+export const PORT = Number(Deno.env.get("PORT")) || 3001;
 export const IS_PRODUCTION = Deno.env.get("DENO_ENV") === "production";
