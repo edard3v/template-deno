f65da0a7c583d2cf3837055a1bccce1fd87549d9
edard3v <edard3v@gmail.com>
2025-02-18 06:08:55 -0500

    cambiando DENO_ENV por ENV

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,7 +1,7 @@
 import { cors } from "hono/cors";
 
 export const PORT = Number(Deno.env.get("PORT")) || 3001;
-export const IS_PRODUCTION = Deno.env.get("DENO_ENV") === "production";
+export const IS_PRODUCTION = Deno.env.get("ENV") === "production";
 export const CORS = cors({
   origin: ["*"],
   allowMethods: ["POST", "GET", "UPDATE", "PUT", "DELETE"],
