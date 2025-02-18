510ff4211f1c36b42e81cb3abdac2c83324bb1ed
edard3v <edard3v@gmail.com>
2025-02-18 05:13:56 -0500

    funcional remote y local

--- src/db/db.ts
+++ src/db/db.ts
@@ -1,8 +1,10 @@
 import { drizzle } from "drizzle-orm/neon-http";
+import { drizzle as drizzle_local } from "drizzle-orm/node-postgres";
 import { neon } from "@neondatabase/serverless";
 import { DATABASE_URL } from "../../drizzle.config.ts";
+import { IS_PRODUCTION } from "../app/config.ts";
 // import * as relations from "./relations.ts";
 
 const sql = neon(DATABASE_URL);
 
-export const db = drizzle({ client: sql });
+export const db = !IS_PRODUCTION ? drizzle_local(DATABASE_URL) : drizzle({ client: sql });
