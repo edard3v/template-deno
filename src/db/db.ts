7f2b353981227bbe182b7175166f9bcca2949e65
edard3v <edard3v@gmail.com>
2025-02-18 05:24:43 -0500

    refactor

--- src/db/db.ts
+++ src/db/db.ts
@@ -3,8 +3,11 @@ import { drizzle as drizzle_local } from "drizzle-orm/node-postgres";
 import { neon } from "@neondatabase/serverless";
 import { DATABASE_URL } from "../../drizzle.config.ts";
 import { IS_PRODUCTION } from "../app/config.ts";
+import * as schema from "./schema.ts";
 // import * as relations from "./relations.ts";
 
 const sql = neon(DATABASE_URL);
 
-export const db = !IS_PRODUCTION ? drizzle_local(DATABASE_URL) : drizzle({ client: sql });
+export const db = !IS_PRODUCTION
+  ? drizzle_local(DATABASE_URL, { schema })
+  : drizzle(sql, { schema });
