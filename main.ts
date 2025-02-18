ea7b963c11561a61178842603c750377010180b2
edard3v <edard3v@gmail.com>
2025-02-18 06:04:28 -0500

    refactor

--- main.ts
+++ main.ts
@@ -1,10 +1,11 @@
 import { app } from "./src/app/app.ts";
-import { PORT } from "./src/app/config.ts";
+import { IS_PRODUCTION, PORT } from "./src/app/config.ts";
 
 Deno.serve(
   {
     port: PORT,
     onListen() {
+      console.log("¿Es producción?: ", IS_PRODUCTION);
       console.log(`Servidor escuchando en http://localhost:${PORT}/`);
     },
   },
