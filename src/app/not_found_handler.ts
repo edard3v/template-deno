60684c708bd7e24890d32a9d2da452813f0ff388
edard3v <edard3v@gmail.com>
2025-02-19 23:30:27 -0500

    refactor

--- src/app/not_found_handler.ts
+++ src/app/not_found_handler.ts
@@ -2,5 +2,5 @@ import { NotFoundHandler } from "hono/types";
 
 export const not_found_handler: NotFoundHandler = (context) => {
   context.status(404);
-  return context.json({ msg: "Ruta no encontrada" });
+  return context.text("Ruta no encontrada");
 };
