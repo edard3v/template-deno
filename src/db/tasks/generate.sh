33994a363f4b5cba9e2aa51fb89e354f38b61e55
edard3v <edard3v@gmail.com>
2025-04-11 18:06:11 -0500

    refactor

--- src/db/tasks/generate.sh
+++ src/db/tasks/generate.sh
@@ -1,8 +1,5 @@
 # Elimina la carpeta de migraciones
 rm -rf src/db/migrations
 
-# Elimina la db
-rm -rf src/db/template.db
-
 # Genera una nueva migración
 deno -A --node-modules-dir npm:drizzle-kit generate
